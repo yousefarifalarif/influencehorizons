1940b150e4d620c8f35495abca12e7c40b221871
Katie Lin <katiekklht@gmail.com>
2021-11-23 11:46:33 +0000

    update controllers

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,42 @@
 class CampaignsController < ApplicationController
+  before_action set_business, only: %i[new create]
+  before_action find_campaign, only: %i[show edit update]
+
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+    @campaign.business = @business
+    if @campaign.save
+      redirect_to campaign_path(@campaign)
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @campaign.update(campaign_params)
+    redirect_to campaign_path(@campaign)
+  end
+
+
+  private
+
+  def set_business
+    @business = Business.find(params[:business_id])
+  end
+
+  def find_campaign
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def campaign_params
+    params.require(:campaign).permit(:name, :location, :start_date, :end_date, :description, :budget)
+  end
 end
--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,2 +1,42 @@
 class ProposalsController < ApplicationController
+  before_action set_campaign, only: %i[new create]
+  before_action find_proposal, only: %i[show edit update]
+
+  def new
+    @proposal = Proposal.new
+  end
+
+  def create
+    @proposal = Proposal.new(proposal_params)
+    @proposal.campaign = @campaign
+    @proposal.user = current_user
+    if @proposal.save
+      redirect_to proposal_path(@proposal)
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @proposal.update(proposal_params)
+    redirect_to proposal_path(@proposal)
+  end
+
+  private
+
+  def set_campaign
+    @campaign = Campaign.find(params[:campaign_id])
+  end
+
+  def find_proposal
+    @proposal = Proposal.find(params[:id])
+  end
+
+  def proposal_params
+    params.require(:proposal).permit(:influencer)
+  end
 end
