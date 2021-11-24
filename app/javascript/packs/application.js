d185ad04052e27addcd4ae95b32465aeb91fa564
yousef arif <yousefarifagha@gmail.com>
2021-11-24 17:41:46 +0000

    side bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,22 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
