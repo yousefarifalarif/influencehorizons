eba227fd0de25aaa03f516c8d755278ce2ba70a6
yousef arif <yousefarifagha@gmail.com>
2021-11-25 18:07:49 +0000

    influencer index

--- db/schema.rb
+++ db/schema.rb
@@ -57,10 +57,10 @@ ActiveRecord::Schema.define(version: 2021_11_24_114545) do
 
   create_table "proposals", force: :cascade do |t|
     t.string "title"
-    t.string "status"
-    t.boolean "accepted"
+    t.string "status", default: "pending"
+    t.boolean "accepted", default: false
     t.bigint "campaign_id", null: false
-    t.bigint "influencer_id", null: false
+    t.bigint "influencer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "creator"
