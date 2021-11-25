092f5992b5302216226360d3f10bc7c4ead93556
Katie Lin <katiekklht@gmail.com>
2021-11-25 13:20:09 +0000

    fixed sidebar and navbar

--- app/javascript/controllers/side_bar_controller.js
+++ app/javascript/controllers/side_bar_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["sidebar"]
+  static targets = ["sidebar", "button", "main"]
   toggle(event) {
-    console.log(event.currentTarget)
-    this.sidebarTarget.classList.toggle('sidebar-expanded')
+    this.sidebarTarget.classList.toggle('sidebar-shrinked')
+    this.mainTarget.classList.toggle('main-expanded')
+    this.buttonTarget.classList.toggle('button-to-left')
+    this.buttonTarget.classList.toggle('fa-angle-left')
+    this.buttonTarget.classList.toggle('fa-angle-right')
+
   }
 }
