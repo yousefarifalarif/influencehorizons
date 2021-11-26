2b1ff52a3423439af18c0a1019f60e5f1980e25d
yousef arif <yousefarifagha@gmail.com>
2021-11-26 15:25:52 +0000

    multi select

--- app/javascript/controllers/influencer_controller.js
+++ app/javascript/controllers/influencer_controller.js
@@ -2,14 +2,24 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
-  static targets = ["product", "btn"]
+  static targets = ["product", "btn", "cards"]
 
   connect() {
     console.log(this.productTarget);
   }
 
-  toggleDisable() {
-    this.btnTarget.setAttribute("disabled", false);
-    this.productTarget.setAttribute("selected", true);
+  toggleDisable(event) {
+    // this.btnTarget.setAttribute("disabled", false);
+    const id = event.target.dataset.id
+    let card = [...this.productTarget.children].filter(child => parseInt(child.dataset.id) == parseInt(id))[0]
+    console.log(card)
+    card.setAttribute("selected", true);
+    // card.classList.toggle("card-product");
+    card.classList.toggle("selected");
+    this.btnTarget.classList.toggle("proposal-btn-active");
+    // const array = []
+    // document.querySelectorAll('.selected').forEach(card => array.push(card.dataset.id))
+    // this.btnTarget.dataset.id = array
+    this.btnTarget.dataset.id = card.dataset.id
   }
 }
