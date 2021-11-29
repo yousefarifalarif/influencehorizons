0b233ebbbf59d97d897ef0adc2464e2154e2f09f
Max Kern <m.kern@ingwiest.de>
2021-11-29 13:15:03 +0000

    campaign details css finalized

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,10 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   updateNavbar() {
     if (window.scrollY >= window.innerHeight * 0.2) {
-      this.element.classList.add('navbar-lewagon-white');
+      this.element.classList.add('navbar-lewagon-blue');
       this.element.classList.add('navbar-light');
     } else {
-      this.element.classList.remove('navbar-lewagon-white');
+      this.element.classList.remove('navbar-lewagon-blue');
       this.element.classList.remove('navbar-light');
     }
   };
