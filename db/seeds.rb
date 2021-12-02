612ec6723f9faad7ff6fe2ab72dbd186ff9467a7
Ethan Hardwick <hardwick.ethan@outlook.com>
2021-12-02 16:51:40 +0000

    updated seed file yet again

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,19 @@ require "open-uri"
 
 User.destroy_all
 
-influencer_avatars = [
+female_influencer_avatars = [
   "https://images.unsplash.com/photo-1605405748313-a416a1b84491?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODQ3MTgw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
-  "https://images.unsplash.com/photo-1570158268183-d296b2892211?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8ZmFjZXx8fHx8fDE2Mzc4NDcyNTk&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
   "https://images.unsplash.com/photo-1578489758854-f134a358f08b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8ZmFjZXx8fHx8fDE2Mzc4NDcyMjQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
-  "https://images.unsplash.com/photo-1534399315465-2b91232de345?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY0NzE0&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
-  "https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY0NzIx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
-  "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY0OTg3&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
-  "https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY1MDE5&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
-  "https://images.unsplash.com/photo-1554151228-14d9def656e4?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY1MDA2&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
-  "https://images.unsplash.com/photo-1543357530-d91dab30fa97?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY1MDE2&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
   "https://images.unsplash.com/photo-1514846326710-096e4a8035e0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY1MDU1&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
   "https://images.unsplash.com/photo-1607503873903-c5e95f80d7b9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY1MDY2&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+]
+
+male_influencer_avatars = [
+  "https://images.unsplash.com/photo-1570158268183-d296b2892211?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8ZmFjZXx8fHx8fDE2Mzc4NDcyNTk&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
+  "https://images.unsplash.com/photo-1534399315465-2b91232de345?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY0NzE0&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
+  "https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY0NzIx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
+  "https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cG9ydHJhaXR8fHx8fHwxNjM3ODY1MDE5&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+
 ]
 
 yousef_avatar = "https://ca.slack-edge.com/T02NE0241-U02GSM7UGAV-9448e7c7069f-512"
@@ -30,17 +31,43 @@ BOOLEANS = [true, false]
 TASKS = ["Do a live feed for", "Design a poster for", "Design a banner for", "Take a photo for",
          "Make a video for", "Create a contest for"]
 INFLUENCERS = []
-live_users = [{first_name: 'Kenzie', last_name: 'Jaylyn', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
-              {first_name: 'Melva', last_name: 'Madilyn', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
+
+female_users = [{first_name: 'Kenzie', last_name: 'Jaylyn', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer"},
               {first_name: 'Paulene', last_name: 'Jewel', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
-              {first_name: 'Zac', last_name: 'Saylor', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
-              {first_name: 'Eben', last_name: 'Kenzie', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
               {first_name: 'Moriah', last_name: 'Wilfred', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
-              {first_name: 'Esther', last_name: 'Millie', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
+              {first_name: 'Esther', last_name: 'Millie', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },]
+
+
+male_users = [{first_name: 'Zac', last_name: 'Saylor', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
+              {first_name: 'Eben', last_name: 'Kenzie', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
               {first_name: 'Noel', last_name: 'Jaylyn', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" },
-              {first_name: 'Serena', last_name: 'Annabel', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" } ]
+              {first_name: 'John', last_name: 'Madilyn', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer" }]
 
 demo_influencers = {first_name: 'Yousef', last_name: 'Arif', password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164, location: "London", role: "Influencer", email: "yousef@gmail.com" }
+
+def create_influences(users, avatars)
+  users.each_with_index do |user, index|
+  puts "Creating Influencer ..."
+    new_user = User.create!(email: "#{user[:first_name]}.#{user[:last_name]}@gmail.com", password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+                        first_name: user[:first_name], last_name: user[:last_name], location: "London", role: "Influencer")
+    username = "#{user[:first_name].chars.first}_#{user[:last_name]}"
+    i_followers = rand(1500..200_000)
+    t_followers = rand(i_followers * 0.5..i_followers)
+    y_followers = rand(i_followers * 0.5..i_followers)
+    f_followers = rand(i_followers * 0.5..i_followers)
+    influencer = Influencer.new(ig_username: username, ig_followers: i_followers,
+                                youtube_channel_name: username, youtube_subscribers: y_followers,
+                                twitter_username: username, twitter_followers: t_followers,
+                                facebook_username: username, facebook_followers: f_followers,
+                                gender: index.even? ? 'Female' : 'Male', estimated_price: i_followers / 1000)
+    file = URI.open(avatars[index])
+    influencer.photo.attach(io: file, filename: "#{username}.png", content_type: 'image/png')
+    influencer.user = new_user
+    influencer.save!
+    INFLUENCERS << influencer
+  end
+end
+
 
 def create_yousef(demo_influencers, yousef_avatar)
   puts "Creating Yousef"
@@ -90,72 +117,53 @@ def create_yousef_proposals(campaign, yousef)
 
 end
 
-# Create random Influencers
-puts "Creating Influencers ..."
-live_users.each_with_index do |user, index|
-  new_user = User.create!(email: "#{user[:first_name]}.#{user[:last_name]}@gmail.com", password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164,
-                      first_name: user[:first_name], last_name: user[:last_name], location: "London", role: "Influencer")
-  username = "#{user[:first_name].chars.first}_#{user[:last_name]}"
-  i_followers = rand(1500..200_000)
-  t_followers = rand(i_followers * 0.5..i_followers)
-  y_followers = rand(i_followers * 0.5..i_followers)
-  f_followers = rand(i_followers * 0.5..i_followers)
-  influencer = Influencer.new(ig_username: username, ig_followers: i_followers,
-                              youtube_channel_name: username, youtube_subscribers: y_followers,
-                              twitter_username: username, twitter_followers: t_followers,
-                              facebook_username: username, facebook_followers: f_followers,
-                              gender: ['Male', 'Female'].sample, estimated_price: i_followers / 1000)
-  file = URI.open(influencer_avatars[index])
-  influencer.photo.attach(io: file, filename: "#{username}.png", content_type: 'image/png')
-  influencer.user = new_user
-  influencer.save!
-  INFLUENCERS << influencer
-end
+YOUSEF = create_yousef(demo_influencers, yousef_avatar)
 
 # Create 4 Businesses
-puts "Creating Businesses ..."
-emails = ["m.kern@ingwiest.de", "hardwick.ethan@outlook.com", "yousef.arif@gmail.com", "katiekklht@gmail.com"]
-YOUSEF = create_yousef(demo_influencers, yousef_avatar)
-CREATED_PROPOSAL = false;
-4.times do |index|
-  puts "Creating Business #{index + 1} ..."
-  user = User.create!(email: emails[index], password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164,
-                      first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, location: "london", role: "Business")
-  business = Business.new(company_name: Faker::Company.name, company_url: Faker::Internet.url, industry: Faker::Company.industry)
-  url = Faker::Company.logo
-  file = URI.open(url)
-  business.photo.attach(io: file, filename: "#{business.company_name}.png", content_type: 'image/png')
-  business.user = user
-  business.save!
+def create_businesses
+  create_proposalr = false;
+  puts "Creating Businesses ..."
+  emails = ["m.kern@ingwiest.de", "hardwick.ethan@outlook.com", "yousef.arif@gmail.com", "katiekklht@gmail.com"]
+  4.times do |index|
+    puts "Creating Business #{index + 1} ..."
+    user = User.create!(email: emails[index], password: "123456", phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+                        first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, location: "london", role: "Business")
+    business = Business.new(company_name: Faker::Company.name, company_url: Faker::Internet.url, industry: Faker::Company.industry)
+    url = Faker::Company.logo
+    file = URI.open(url)
+    business.photo.attach(io: file, filename: "#{business.company_name}.png", content_type: 'image/png')
+    business.user = user
+    business.save!
 
-  # Create 1-2 Campaign for each Business
-  puts "Creating Campaigns & Proposals for Business #{index + 1} ..."
-  rand(4..8).times do
-    date = Faker::Date.forward(days: rand(10..30))
-    campaign = Campaign.new(name: Faker::Commerce.product_name, location: "London", start_date: date,
-                            end_date: date + rand(15..45), description: Faker::Marketing.buzzwords, budget: Faker::Commerce.price(range: 500..1000))
-    campaign.archived = index.zero? ? true : BOOLEANS.sample
-    campaign.business = business
-    campaign.save!
+    # Create 1-2 Campaign for each Business
+    puts "Creating Campaigns & Proposals for Business #{index + 1} ..."
+    rand(4..8).times do
+      date = Faker::Date.forward(days: rand(10..30))
+      campaign = Campaign.new(name: Faker::Commerce.product_name, location: "London", start_date: date,
+                              end_date: date + rand(15..45), description: Faker::Marketing.buzzwords, budget: Faker::Commerce.price(range: 500..1000))
+      campaign.archived = index.zero? ? true : BOOLEANS.sample
+      campaign.business = business
+      campaign.save!
 
-    # Create 1-3 Proposals for each Campaign
-    rand(1..4).times do
-      if CREATED_PROPOSAL == false
-        create_yousef_proposals(campaign, YOUSEF)
-        CREATED_PROPOSAL = true
+      # Create 1-3 Proposals for each Campaign
+      rand(1..4).times do
+        if create_proposalr == false
+          create_yousef_proposals(campaign, YOUSEF)
+          create_proposalr = true
+        end
+        if campaign.archived
+          proposal = Proposal.new(title: "#{TASKS.sample} #{campaign.name}", creator: "Business", status: "Completed")
+        else
+          proposal = Proposal.new(title: "#{TASKS.sample} #{campaign.name}", creator: %w[Business Influencer].sample)
+          proposal.status = proposal.creator == "Influencer" ? "Pending" : STATUSES.sample
+        end
+        proposal.accepted = proposal.status != "Pending"
+        proposal.campaign = campaign
+        proposal.influencer = INFLUENCERS.sample
+        proposal.save!
+
+        chatroom = Chatroom.create!(name: proposal.title, proposal:proposal)
       end
-      if campaign.archived
-        proposal = Proposal.new(title: "#{TASKS.sample} #{campaign.name}", creator: "Business", status: "Completed")
-      else
-        proposal = Proposal.new(title: "#{TASKS.sample} #{campaign.name}", creator: %w[Business Influencer].sample)
-        proposal.status = proposal.creator == "Influencer" ? "Pending" : STATUSES.sample
-      end
-      proposal.accepted = proposal.status != "Pending"
-      proposal.campaign = campaign
-      proposal.influencer = INFLUENCERS.sample
-      proposal.save!
-
-      chatroom = Chatroom.create!(name: proposal.title, proposal:proposal)
     end
   end
 end
@@ -204,6 +212,26 @@ def create_campaigns(campaign, business)
     end
   end
 end
+
+
+def merge_influencers(female_influencer_avatars, male_influencer_avatars, female_users, male_users)
+  influencer_avatars = []
+  users =[]
+  4.times do |index|
+    influencer_avatars << female_influencer_avatars[index]
+    users << female_users[index]
+    influencer_avatars << male_influencer_avatars[index]
+    users << male_users[index]
+  end
+
+  create_influences(users, influencer_avatars)
+end
+
+# Merge and create random influencers
+merge_influencers(female_influencer_avatars, male_influencer_avatars, female_users, male_users)
+
+# Create 4 businesses
+create_businesses
 
 # creating The Only Socks as a business
 create_socks_business
